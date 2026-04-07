5916d0c6475645dd5fe51d2fecb3996fc22ec806
dannyho-dev <dho67@my.bcit.ca>
2026-04-07 13:51:26 -0700

    fixed pathing

--- scripts/Sagat.js
+++ scripts/Sagat.js
@@ -11,18 +11,18 @@ export class Sagat extends Fighter {
         const hurtAnims = [];
 
         for (let i = 1; i <= numAttackAnims; i++) {
-            attackAnims.push(`/2132-project/images/${name.toLowerCase()}/${name.toLowerCase()}-fight${i}.png`);
+            attackAnims.push(`./images/${name.toLowerCase()}/${name.toLowerCase()}-fight${i}.png`);
         }
         for (let i = 1; i <= numHurtAnims; i++) {
-            hurtAnims.push(`/2132-project/images/${name.toLowerCase()}/${name.toLowerCase()}-hurt${i}.png`);
+            hurtAnims.push(`./images/${name.toLowerCase()}/${name.toLowerCase()}-hurt${i}.png`);
         }
 
         super({
             attack: attackAnims,
             hurt: hurtAnims,
-            idle: `/2132-project/images/${name.toLowerCase()}/${name.toLowerCase()}-idle.gif`,
-            defeat: `/2132-project/images/${name.toLowerCase()}/${name.toLowerCase()}-defeat.png`,
-            victory: `/2132-project/images/${name.toLowerCase()}/${name.toLowerCase()}-victory.gif`,
+            idle: `./images/${name.toLowerCase()}/${name.toLowerCase()}-idle.gif`,
+            defeat: `./images/${name.toLowerCase()}/${name.toLowerCase()}-defeat.png`,
+            victory: `./images/${name.toLowerCase()}/${name.toLowerCase()}-victory.gif`,
         });
     }
 }
